13e2646cc272d96ba9ef7cd9a962923c9248ed86
wayneleon1 <rurangwaleon10@gmail.com>
2025-10-02 09:24:35 +0200

    Using Flutter package(http)

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,25 +8,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  void getData() async {
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      return "Yoshi";
-    });
-
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return "Musician";
-    });
-
-    print('$username  - $bio');
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getData();
-    print('Hey there!');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +10,23 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void getData() async {
+    Response response = await get(
+      Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
+      headers: {'Accept': 'application/json'},
+    );
+
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Text('Loading Screen'));
